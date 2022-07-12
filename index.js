b91736fc1b74818bae34b99c744d097bfd427a70
ram-9047 <ramg786@live.com>
2022-07-13 00:24:30 +0530

    getColdDrink function added

--- index.js
+++ index.js
@@ -7,9 +7,57 @@ let promiseWIfeBringTickets = new Promise((resolve, reject) => {
   }, 3000);
 });
 
-promiseWIfeBringTickets.then(function ticketBought(ticket) {
-  console.log(`person3 : shows ${ticket}`);
+// let getSomePopcorn = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("popcorn");
+//   }, 2000);
+// });
+
+// let getColdDrinks = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("coke");
+//   }, 2000);
+// });
+
+let getPopcorn = promiseWIfeBringTickets.then(function boughtTicket(ticket) {
+  console.log("husband: we should go in.");
+  console.log("wife: i'm hungry");
+
+  return new Promise((resolve, reject) => {
+    // resolve(`got the popcorn`)
+    setTimeout(() => {
+      resolve(`${ticket} and popcorn`);
+    }, 2000);
+  });
 });
+
+let getButter = getPopcorn.then(function boughtPopcorn(popcorn) {
+  console.log("husband: we should go in..");
+  console.log("wife: i need butter");
+  //   console.log(`here is your ${popcorn}`);
+  return new Promise((resolve, reject) => {
+    resolve(`${popcorn} with butter`);
+  });
+});
+
+let getColdDrink = getButter.then(function boughtButter(butter) {
+    console.log("husband: anything else baby");
+    console.log("wife: cold drinks !!!");
+    
+    return new Promise((resolve,reject) => {
+        // resolve(`${butter} and coke`);
+        setTimeout(()=>{
+            resolve(`${butter} and coke`);
+        },1000)
+    })
+//   console.log(`here is your ${butter}`);
+});
+
+getColdDrink.then(function boughtDrinks(drinks){
+    console.log(`Husband: here is your${drinks}`);
+    console.log('husband: lets go, we are getting late');
+    console.log("wife: ya lets go");
+})
 
 console.log("person4: shows ticket");
 console.log("person5: shows ticket");
